6b291b86f307440a7519c6ae679ba5c8165a2253
Yaroslav Korotaev <yaroslav.korotaev@gmail.com>
2024-06-03 03:36:48 +0300

    new approach

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,40 +1,58 @@
-import { type AsyncFunction, type SpanCallback, type Tracing } from 'turbotrace';
+import {
+  type Log,
+  type Metrics,
+  type Tracing,
+  type Telemetry,
+} from 'universe-types';
 
 export type TelemetryParams = {
+  log: Log;
+  metrics: Metrics;
   tracing: Tracing;
-};
-
-export type Telemetry = {
-  (msg?: string): void;
-  (details?: object, msg?: string): void;
-  
-  tracing: Tracing;
-  
-  child(name: string): Telemetry;
-  
-  span<T>(name: string, callback: SpanCallback<T>): Promise<T>;
-  wrap<T, A extends any[], R>(name: string, fn: AsyncFunction<T, A, R>): AsyncFunction<T, A, R>;
-  trace(msg?: string): void;
-  trace(details?: object, msg?: string): void;
 };
 
 export function createTelemetry(params: TelemetryParams): Telemetry {
   const {
+    log,
+    metrics,
     tracing,
   } = params;
   
-  const _self: Telemetry = tracing.trace.bind(tracing) as Telemetry;
+  const _self: Telemetry = () => {};
   
+  _self.log = log;
+  _self.metrics = metrics;
   _self.tracing = tracing;
   
-  _self.child = name => {
+  _self.destroy = metrics.destroy.bind(metrics);
+  _self.child = (tag, options) => {
+    const {
+      labels,
+      details,
+    } = options ?? {};
+    
+    const taggedLabels = { tag: `${tracing.tag}.${tag}`, ...labels };
+    
     return createTelemetry({
-      tracing: tracing.child(name),
+      log: log.child({ labels: taggedLabels, details }),
+      metrics: metrics.child(taggedLabels),
+      tracing: tracing.child(tag),
     });
   };
   
+  _self.debug = log.debug.bind(log);
+  _self.info = log.info.bind(log);
+  _self.warn = log.warn.bind(log);
+  _self.error = log.error.bind(log);
+  
+  _self.counter = metrics.counter.bind(metrics);
+  _self.gauge = metrics.gauge.bind(metrics);
+  _self.histogram = metrics.histogram.bind(metrics);
+  _self.summary = metrics.summary.bind(metrics);
+  
   _self.span = tracing.span.bind(tracing);
   _self.wrap = tracing.wrap.bind(tracing);
+  
   _self.trace = tracing.trace.bind(tracing);
   
   return _self;
